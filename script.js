03dedfc7d90bcc3d2a4de92c5c1332b35e44a408
rousanjronline-prog <rousanjronline@gmail.com>
2026-05-03 03:23:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-// MIRROR BREAK INTRO
-
-setTimeout(()=>{
-
-const intro = document.getElementById("intro");
-const site = document.getElementById("site");
-
-intro.style.transition = "1s";
-intro.style.transform = "scale(2) rotate(15deg)";
-intro.style.opacity = "0";
-
-site.style.opacity = "1";
-site.style.transform = "scale(1)";
-
-document.body.style.overflow = "auto";
-
-setTimeout(()=>{
-intro.remove();
-},1000);
-
-},3500);
-
-
-// MENU
+// script.js
 
 const menuBtn = document.getElementById("menuBtn");
-const sideMenu = document.getElementById("sideMenu");
-const closeBtn = document.getElementById("closeBtn");
-const overlay = document.getElementById("overlay");
 
-menuBtn.onclick = ()=>{
-sideMenu.classList.add("show");
-overlay.classList.add("show");
-};
-
-closeBtn.onclick = ()=>{
-sideMenu.classList.remove("show");
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+});sideMenu.classList.remove("show");
 overlay.classList.remove("show");
 };
 
