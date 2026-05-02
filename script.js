20609342887f5af819b7ad76f4eb258cc0cc4509
rousanjronline-prog <rousanjronline@gmail.com>
2026-05-03 03:31:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,4 @@ const menuBtn = document.getElementById("menuBtn");
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
-});sideMenu.classList.remove("show");
-overlay.classList.remove("show");
-};
-
-overlay.onclick = ()=>{
-sideMenu.classList.remove("show");
-overlay.classList.remove("show");
-};
-
-
-// RANDOM GLITCH BURST
-
-const glitch = document.querySelector(".glitch");
-
-setInterval(()=>{
-
-glitch.style.transform = "skew(6deg) scale(1.02)";
-
-setTimeout(()=>{
-glitch.style.transform = "";
-},120);
-
-},2500);
+});
