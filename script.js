596e3f9bbb7abfa3e4dc0bf3982be8d5e629b2ca
rousanjronline-prog <rousanjronline@gmail.com>
2026-05-03 13:21:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 window.addEventListener('load',()=>{
- setTimeout(()=>document.getElementById('loader').style.display='none',1200);
+  setTimeout(()=>document.getElementById('loader').style.display='none',1200);
 });
+const btn=document.querySelector('.menu-btn');
+const menu=document.querySelector('.mobile-menu');
+btn.addEventListener('click',()=>menu.classList.toggle('show'));
 
 document.querySelectorAll('a[href^="#"]').forEach(link=>{
- link.addEventListener('click',e=>{
-  e.preventDefault();
-  document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
- });
+  link.addEventListener('click',e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+    menu.classList.remove('show');
+  });
 });
 
 document.querySelector('form').addEventListener('submit',e=>{
